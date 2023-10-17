905199f0330f27a8182f0fdd48e37a4cd9acefa3
PrangonGhose <prangonghose@gmail.com>
2023-10-17 23:33:51 +0200

    Update calorie counter function to use JSON file

--- calorie_counter.py
+++ calorie_counter.py
@@ -1,18 +1,23 @@
-def calorie_counter(*args):
+from load_data import load_data
 
-  calories = {
-    'Hamburger': 600,
-    'Cheese Burger': 750,
-    'Veggie Burger': 400,
-    'Vegan Burger': 350,
-    'Sweet Potatoes': 230,
-    'Salad': 15,
-    'Iced Tea': 70,
-    'Lemonade': 90,
-  } # calories dictionary
+def calorie_counter(orders):
+  from combo_calories_counter import combo_calories_counter
+
+  all_meals = load_data("data/meals.json")
+  meals = all_meals['meals']
+
+  all_combos = load_data("data/combos.json")
+  combos = all_combos['combos']
 
   total = 0
 
-  for arg in args:
-    total += calories[arg] if arg in calories else 0 # if arg in calories, add calories[arg] to total, else add 0 to total
+  for order in orders:
+    if 'combo' in order:
+      for combo in combos:
+        if order == combo['name']:
+          total += combo_calories_counter(order)
+    else:
+      for meal in meals:
+        if order == meal['name'] or order == meal['id']:
+          total += meal['calories']
   return total
